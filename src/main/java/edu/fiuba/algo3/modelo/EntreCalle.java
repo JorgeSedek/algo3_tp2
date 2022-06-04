1f9dc496bdbf9d1eb09b7f9edc13d70ab8c4f163
MartinAlejo <mpolese@fi.uba.ar>
2022-06-04 16:58:07 -0300

    Agregar la clase Calle y agregar comportamiento a EntreCalle

--- src/main/java/edu/fiuba/algo3/modelo/EntreCalle.java
+++ src/main/java/edu/fiuba/algo3/modelo/EntreCalle.java
@@ -1,5 +1,27 @@
 package edu.fiuba.algo3.modelo;
 
 public class EntreCalle {
+    private Calle arriba;
+    private Calle izquierda;
+    private Calle derecha;
+    private Calle abajo;
+    public EntreCalle () {
+        this.arriba = null;
+        this.abajo = null;
+        this.izquierda = null;
+        this.derecha = null;
+    }
+    public void AsignarCalleSuperior (Calle superior) {
+        this.arriba = superior;
+    }
+    public void AsignarCalleInferior (Calle inferior) {
+        this.abajo = inferior;
+    }
+    public void AsignarCalleIzquierda (Calle izquierda) {
+        this.izquierda = izquierda;
+    }
+    public void AsignarCalleDerecha (Calle derecha) {
+        this.derecha = derecha;
+    }
 
 }
