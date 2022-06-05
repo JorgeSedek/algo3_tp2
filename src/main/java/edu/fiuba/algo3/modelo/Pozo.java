5f059c7460c32deb8771d89578725fb51115196a
Jorge <jorgesedek@gmail.com>
2022-06-05 16:10:48 -0300

    Se crea pozo y testPozo Simplificado con camioneta

---
+++ src/main/java/edu/fiuba/algo3/modelo/Pozo.java
@@ -0,0 +1,11 @@
+package edu.fiuba.algo3.modelo;
+
+public class Pozo {
+	public int pasarObstaculo(Auto unAuto) {
+		return 3;
+	}
+	
+	public int pasarObstaculo(CuatroXCuatro unaCamioneta) {
+		return 2;
+	}
+}
---
+++ src/main/test/edu/fiuba/algo3/modelo/PozoTest.java
@@ -0,0 +1,33 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+
+import edu.fiuba.algo3.modelo.Pozo;
+import edu.fiuba.algo3.modelo.Vehiculo;
+
+
+
+public class PozoTest {
+	@Test
+	public void pasarObstaculoConAutoDevuelve3movimientos() {
+		Pozo pozo = new Pozo();
+		Auto auto = new Auto();
+		
+		int movimientos = pozo.pasarObstaculo(auto);
+		assertEquals(3,movimientos);
+	}
+	
+	@Test
+	public void pasarObstaculoConCuatroXCuatroDevuelve2movimientos() {
+		Pozo pozo = new Pozo();
+		CuatroXCuatro camioneta = new CuatroXCuatro();
+		
+		int movimientos = pozo.pasarObstaculo(camioneta);
+		assertEquals(2,movimientos);
+	}
+}
