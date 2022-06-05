f4e10ae2612d025a44b12b00eb966a85893866af
Jorge <jorgesedek@gmail.com>
2022-06-04 22:30:11 -0300

    cambio imports para arreglar errores en test

--- src/main/test/edu/fiuba/algo3/modelo/EscenarioTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/EscenarioTest.java
@@ -2,9 +2,11 @@ package edu.fiuba.algo3.modelo;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 
 import edu.fiuba.algo3.modelo.Escenario;
 import edu.fiuba.algo3.modelo.EntreCalle;
