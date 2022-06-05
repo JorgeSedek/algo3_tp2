bc103fcb52b1639a3d4308d50c98a00ea81260c2
MartinAlejo <mpolese@fi.uba.ar>
2022-06-05 03:23:16 -0300

    Cambios al test de vehiculo

--- src/main/test/edu/fiuba/algo3/modelo/VehiculoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/VehiculoTest.java
@@ -1,13 +1,12 @@
 package edu.fiuba.algo3.modelo;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class VehiculoTest {
 
     @Test
-    void seActualizoLaUbicacionDelVehiculo () {
+    public void seActualizoLaUbicacionDeLaMoto () {
         EntreCalle una_entrecalle = new EntreCalle();
         EntreCalle otra_entreCalle = new EntreCalle();
         Moto moto = new Moto();
@@ -15,5 +14,27 @@ public class VehiculoTest {
         moto.nuevaUbicacion(una_entrecalle);
         assertEquals(moto.obtenerUbicacion(), una_entrecalle);
         assertNotEquals(moto.obtenerUbicacion(), otra_entreCalle);
+    }
+
+    @Test
+    public void seActualizoLaUbicacionDelAuto () {
+        EntreCalle una_entrecalle = new EntreCalle();
+        EntreCalle otra_entreCalle = new EntreCalle();
+        Auto auto = new Auto();
+
+        auto.nuevaUbicacion(una_entrecalle);
+        assertEquals(auto.obtenerUbicacion(), una_entrecalle);
+        assertNotEquals(auto.obtenerUbicacion(), otra_entreCalle);
+    }
+
+    @Test
+    public void seActualizoLaUbicacionDeLaCuatroXCuatro () {
+        EntreCalle una_entrecalle = new EntreCalle();
+        EntreCalle otra_entreCalle = new EntreCalle();
+        CuatroXCuatro cuatroxcuatro = new CuatroXCuatro();
+
+        cuatroxcuatro.nuevaUbicacion(una_entrecalle);
+        assertEquals(cuatroxcuatro.obtenerUbicacion(), una_entrecalle);
+        assertNotEquals(cuatroxcuatro.obtenerUbicacion(), otra_entreCalle);
     }
 }
