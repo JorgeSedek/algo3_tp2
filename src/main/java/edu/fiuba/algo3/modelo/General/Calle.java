51b15f5ba796065cad165d663d3584cbb16da6bb
Jorge <jorgesedek@gmail.com>
2022-06-16 15:21:29 -0300

    se elimina implementacion Calle que no se utiliza

--- src/main/java/edu/fiuba/algo3/modelo/General/Calle.java
+++ src/main/java/edu/fiuba/algo3/modelo/General/Calle.java
@@ -9,23 +9,12 @@ public class Calle {
     private Obstaculo obstaculo;
     private Sorpresa sorpresa;
 
-    private Casillero casilleroOrigen;
-
-    private Casillero casilleroDesino;
 
     public Calle() {
         this.obstaculo = null;
         this.sorpresa = null;
     }
-
-    public void asignarOrigenYDestino(Casillero primerCasillero, Casillero segundoCasillero){
-        this.casilleroOrigen = primerCasillero;
-        this.casilleroDesino = segundoCasillero;
-    }
-
-    public boolean equals(Casillero primerCasillero, Casillero segundoCasillero){
-        return (primerCasillero == casilleroOrigen & segundoCasillero == casilleroDesino);
-    }
+    
 
     public Calle(Obstaculo obstaculo, Sorpresa sorpresa) {
         this.obstaculo = obstaculo;
