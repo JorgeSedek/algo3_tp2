dcae140cc68b34a0a51cf08630e652f0013cd39e
MartinAlejo <mpolese@fi.uba.ar>
2022-06-16 19:51:55 -0300

    Arreglo la probabilidad en Policia, va a bajar la cobertura porque faltan los tests.

--- src/main/java/edu/fiuba/algo3/modelo/Obstaculos/Policia.java
+++ src/main/java/edu/fiuba/algo3/modelo/Obstaculos/Policia.java
@@ -5,35 +5,28 @@ import edu.fiuba.algo3.modelo.Vehiculo.Auto;
 import edu.fiuba.algo3.modelo.Vehiculo.Camioneta;
 import edu.fiuba.algo3.modelo.Vehiculo.Moto;
 
-/*
-
-LA PROBABILIDAD NO FUNCIONA (siempre es 0)
-NO SE ESTA UTILIZANDO DE MOMENTO
-
 public class Policia implements Obstaculo {
     public void atravesar(Auto auto) {
-        int probabilidad = (int) Math.random() * 10 + 1;
-        if(probabilidad <= 5) {
+        double probabilidad = Math.random(); // Valor decimal entre 0 y 1
+        if(probabilidad <= 0.5) {
             int incremento = 3;
             auto.incrementarMovimientos(incremento);
         }
     }
 
     public void atravesar(Camioneta camioneta) {
-        int probabilidad = (int) Math.random() * 10 + 1;
-        if(probabilidad <= 3) {
+        double probabilidad = Math.random();
+        if(probabilidad <= 0.3) {
             int incremento = 3;
             camioneta.incrementarMovimientos(incremento);
         }
     }
 
     public void atravesar(Moto moto) {
-        int probabilidad = (int) Math.random() * 10 + 1;
-        if(probabilidad <= 8) {
+        double probabilidad = Math.random();
+        if(probabilidad <= 0.8) {
             int incremento = 3;
             moto.incrementarMovimientos(incremento);
         }
     }
 }
-
-*/
