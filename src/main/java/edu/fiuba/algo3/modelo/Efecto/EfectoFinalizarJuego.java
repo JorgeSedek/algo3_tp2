cdb932c89ed667e65acd4f474200a482c9cc0c70
MartinAlejo <mpolese@fi.uba.ar>
2022-06-28 18:26:24 -0300

    Generalizo el efecto de finalziar juego con el decorator (no cambia nada pero queda mas generalizado con el resto de efectos)

--- src/main/java/edu/fiuba/algo3/modelo/Efecto/EfectoFinalizarJuego.java
+++ src/main/java/edu/fiuba/algo3/modelo/Efecto/EfectoFinalizarJuego.java
@@ -1,10 +1,16 @@
 package edu.fiuba.algo3.modelo.Efecto;
 
 import edu.fiuba.algo3.modelo.General.Juego;
+import edu.fiuba.algo3.modelo.General.Jugador;
 
 public class EfectoFinalizarJuego extends EfectoDecorador {
     public EfectoFinalizarJuego(Efecto efecto) {
         super(efecto);
+    }
+
+    @Override
+    public void aplicar(Jugador jugador) {
+        super.aplicar(jugador);
         Juego.getInstance().finalizar();
     }
 }
