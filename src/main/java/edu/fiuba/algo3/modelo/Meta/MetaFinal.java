5882fd54155c9ae440d96c7f7f2b5533460a0cc6
MartinAlejo <mpolese@fi.uba.ar>
2022-06-29 03:17:33 -0300

    Agrego una posible implementación de MetaFinal comentada (se deja el vehiculo en el casillero anterior a la meta luego de atravesarla)

--- src/main/java/edu/fiuba/algo3/modelo/Meta/MetaFinal.java
+++ src/main/java/edu/fiuba/algo3/modelo/Meta/MetaFinal.java
@@ -3,6 +3,7 @@ package edu.fiuba.algo3.modelo.Meta;
 import edu.fiuba.algo3.modelo.Efecto.Efecto;
 import edu.fiuba.algo3.modelo.Efecto.EfectoFinalizarJuego;
 import edu.fiuba.algo3.modelo.Efecto.EfectoGeneral;
+import edu.fiuba.algo3.modelo.Efecto.EfectoMoverAlReves;
 import edu.fiuba.algo3.modelo.General.Juego;
 import edu.fiuba.algo3.modelo.Vehiculo.Vehiculo;
 
@@ -10,6 +11,7 @@ public class MetaFinal implements Meta{
 
     public Efecto atravesar(Vehiculo vehiculo) {
         Efecto efecto = new EfectoGeneral();
+        //Efecto efecto = new EfectoMoverAlReves(new EfectoGeneral(), vehiculo); Esta es otra implementacion, donde se deja en el casillero anterior a la meta
         return new EfectoFinalizarJuego(efecto);
     }
 }
