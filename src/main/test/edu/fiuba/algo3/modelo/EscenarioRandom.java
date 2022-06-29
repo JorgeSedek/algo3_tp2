d1bb01ad97504b2a1b19b7f097810fcc4aa0748d
MartinAlejo <mpolese@fi.uba.ar>
2022-06-28 21:42:24 -0300

    Agrego un test mas para el escenario random, verificando que la meta se crea correctamente

--- src/main/test/edu/fiuba/algo3/modelo/EscenarioRandom.java
+++ src/main/test/edu/fiuba/algo3/modelo/EscenarioRandom.java
@@ -12,6 +12,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class EscenarioRandom {
@@ -31,12 +32,39 @@ public class EscenarioRandom {
         List<Jugador> jugadores = new ArrayList<>() {{ add(jugador1); }};
 
         // Reseteo el escenario y el juego
-        Escenario.resetInstance(totalFilas, totalColumnas);
+        Escenario.resetInstance(totalFilas, totalColumnas); // De esta manera la meta solo puede estar en un unico lugar (2,3)
         Juego.resetInstance(jugadores);
 
         // Lleno de obstaculos, sorpresas y una meta al escenario
         Escenario.getInstance().asignarColocadorRandom();
         Escenario.getInstance().agregarObstaculosYSorpresas();
         Escenario.getInstance().agregarMeta();
+    }
+
+    @Test
+    public void elEscenarioRandomCreaLaMetaYUnAutoLaAtraviesa() {
+        Ubicacion ubicacion = (new Ubicacion(filaInicial, columnaInicial));
+        Vehiculo auto = new Auto(ubicacion);
+        Jugador jugador1 = new Jugador(nombre, auto);
+        List<Jugador> jugadores = new ArrayList<>() {{ add(jugador1); }};
+
+        // Reseteo el escenario y el juego
+        Escenario.resetInstance(3, 3); // De esta manera la meta solo puede estar en un unico lugar (2,3)
+        Juego.resetInstance(jugadores);
+
+        // Le agrego una meta al escenario (se agrega de forma random, pero por el tamaño va a estar en (2,3)
+        Escenario.getInstance().asignarColocadorRandom();
+        Escenario.getInstance().agregarMeta();
+
+        // Act
+        Juego.getInstance().moverVehiculo(new DireccionDerecha());
+
+        // Assert
+        List<Puntaje> puntajes = Juego.getInstance().obtenerPuntajes();
+        Puntaje puntajeJugador = puntajes.get(0);
+
+        assertEquals(puntajes.size(), 1);
+        assertEquals(puntajeJugador.obtenerNombreJugador(), nombre);
+        assertEquals(puntajeJugador.obtenerPuntuacion(), 1);
     }
 }
