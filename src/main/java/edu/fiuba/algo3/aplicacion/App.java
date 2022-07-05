9aac0e3392370b256d97a5c89a1c266cd5a4dd45
Tomas-NO <tomasnolivera@gmail.com>
2022-07-04 22:11:29 -0300

    Arreglo instrucciones para que se adecue al tamaño

--- src/main/java/edu/fiuba/algo3/aplicacion/App.java
+++ src/main/java/edu/fiuba/algo3/aplicacion/App.java
@@ -95,8 +95,8 @@ public class App extends Application {
         TextArea textArea = new TextArea(
                 "Gameplay\n\n"
                 + "GPS es un juego de estrategia por turnos.\n"
-                + "El escenario es una ciudad y el objetivo, guiar un vehículo a la meta en la menor cantidad de movimientos posibles.\n"
-                + "El juego se jugará por turnos, y en cada turno el usuario decide hacia cual de las 4 esquinas posibles avanzará.\n\n"
+                + "El escenario es una ciudad y el objetivo, guiar un vehículo a la meta en la menor \ncantidad de movimientos posibles.\n"
+                + "El juego se jugará por turnos, y en cada turno el usuario decide hacia cual de las 4 \nesquinas posibles avanzará.\n\n"
 
                 + "Vehiculos\n\n"
                 + "El jugador podrá optar por tres diferentes tipos de vehículos:\n"
@@ -105,22 +105,21 @@ public class App extends Application {
                 + " * Camioneta\n\n"
 
                 + "Obstáculos\n\n"
-                + "Al atravesar una cuadra el jugador se podrá encontrar con alguno de los siguientes obstáculos:\n"
-                + " * Pozo: Le suma 3 movimientos de penalización a autos y motos. Para una 4x4 penaliza en 2 movimientos luego de atravesar 3 pozos.\n"
-                + " * Piquete: Autos y Camionetas deben pegar la vuelta, no pueden pasar. Las motos pueden pasar con una penalización de 2 movimientos.\n"
-                + " * Control Policial: Para todos los vehículos la penalización es de 3 movimientos, pero la probabilidad de que sea aplicada depende del vehículo.\n\n"
+                + "Al atravesar una cuadra el jugador se podrá encontrar con alguno de los \nsiguientes obstáculos:\n"
+                + " * Pozo: Le suma 3 movimientos de penalización a autos y motos. Para una 4x4, \npenaliza en 2 movimientos luego de atravesar 3 pozos.\n"
+                + " * Piquete: Autos y Camionetas deben pegar la vuelta, no pueden pasar. Las motos \npueden pasar con una penalización de 2 movimientos.\n"
+                + " * Control Policial: Para todos los vehículos la penalización es de 3 movimientos, pero \nla probabilidad de que sea aplicada depende del vehículo.\n\n"
 
                 + "Sorpresas\n\n"
                 + "También se podrán encontrar diferentes tipos de sorpresas:\n"
                 + " * Favorable: Resta el 20% de los movimientos hechos.\n"
                 + " * Desfavorable: Suma el 25% de los movimientos hechos.\n"
-                + " * Cambio de Vehículo: Si es una moto, la convierte en auto. Un auto lo convierte en una Camioneta. Una camioneta la convierte en moto.\n\n"
-                + "Las sorpresas figuran en el mapa como un regalo y no se sabrá que es hasta que el vehículo la accione."
+                + " * Cambio de Vehículo: Si es una moto, la convierte en auto. Un auto lo convierte en \nuna Camioneta. Una camioneta la convierte en moto.\n\n"
+                + "Las sorpresas figuran en el mapa como un regalo y no se sabrá que es hasta que el \nvehículo la accione."
         );
 
         textArea.setEditable(false);
-        textArea.setPrefHeight(200);
-        textArea.setPrefWidth(800);
+        textArea.setPrefHeight(800);
 
         Button salir = new Button("Atras");
         BotonSalirElegirJugadoresEvent botonSalir = new BotonSalirElegirJugadoresEvent(this);
@@ -128,20 +127,15 @@ public class App extends Application {
         salir.defaultButtonProperty().bind(salir.focusedProperty());
 
         VBox vbox = new VBox(textArea, salir);
-        vbox.setSpacing(30);
+
         vbox.setAlignment(Pos.CENTER);
 
         layout.getChildren().add(vbox);
-        layout.setPrefHeight(400);
-        layout.setPrefWidth(800);
 
         Scene scene = new Scene(layout);
         scene.getStylesheets().add("file:../algo3_tp2/src/main/java/edu/fiuba/algo3/aplicacion/css/principal.css");
-        this.getStage().setResizable(false);
         this.getStage().centerOnScreen();
         this.getStage().setScene(scene);
-
-
     }
 
     public Stage getStage(){
